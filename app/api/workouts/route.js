ff81f453a5e2b670d5f99833a6b4cfbdf37f564a
NoShare16 <alex.wirschke14@gmail.com>
2023-11-14 12:02:21 +0100

    add und delete workouts

--- app/api/workouts/route.js
+++ app/api/workouts/route.js
@@ -61,3 +61,46 @@ export async function GET(request) {
     });
   }
 }
+
+export async function DELETE(request) {
+  await connectDb();
+
+  const { workoutId } = await request.json(); // Assuming workoutId is in the request body
+
+  // Validate the required field
+  if (!workoutId) {
+    return NextResponse.json(
+      { message: "Workout ID is required" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    // Remove the workout from the Category document first
+    await Category.updateOne(
+      { workouts: workoutId },
+      { $pull: { workouts: workoutId } }
+    );
+
+    // Delete the Workout document
+    const deletedWorkout = await Workout.findByIdAndDelete(workoutId);
+
+    if (!deletedWorkout) {
+      return NextResponse.json(
+        { message: "Workout not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Workout deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error in DELETE API:", error);
+    return NextResponse.json(
+      { message: "Error deleting workout", error: error.message },
+      { status: 500 }
+    );
+  }
+}
--- components/Workouts.tsx
+++ components/Workouts.tsx
@@ -19,7 +19,9 @@ interface Workout {
 
 const Workouts: React.FC = () => {
   const [workouts, setWorkouts] = useState<Workout[]>([]);
+  const [newWorkoutName, setNewWorkoutName] = useState("");
   const searchParams = useSearchParams();
+  const categoryId = searchParams.get("categoryId");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -56,11 +58,66 @@ const Workouts: React.FC = () => {
     fetchData();
   }, [searchParams]);
 
+  const createWorkout = async () => {
+    if (!newWorkoutName.trim()) return;
+
+    try {
+      const response = await fetch("/api/workouts", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name: newWorkoutName, categoryId }),
+      });
+
+      if (!response.ok) {
+        console.error("Error creating workout:", response.statusText);
+        return;
+      }
+
+      const newWorkout = await response.json();
+      setWorkouts([...workouts, newWorkout]);
+      setNewWorkoutName("");
+    } catch (error) {
+      console.error("Error adding new workout:", error);
+    }
+  };
+
+  const deleteWorkout = async (workoutId: string) => {
+    try {
+      const response = await fetch(`/api/workouts`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ workoutId }),
+      });
+
+      if (!response.ok) {
+        console.error("Error deleting workout:", response.statusText);
+        return;
+      }
+
+      setWorkouts(workouts.filter((workout) => workout._id !== workoutId));
+    } catch (error) {
+      console.error("Error in deleteWorkout function:", error);
+    }
+  };
+
   return (
     <main className="m-6">
       <h1 className="font-bold text-2xl flex justify-center">
         Workout Collection
       </h1>
+
+      <div className="my-4 border-2 border-solid rounded-xl flex flex-col gap-2 m-4 p-4 ">
+        <input
+          type="text"
+          value={newWorkoutName}
+          onChange={(e) => setNewWorkoutName(e.target.value)}
+          placeholder=" New Workout "
+          className="bg-black "
+        />
+        <button onClick={createWorkout} className="text-yellow-300">
+          Create Workout
+        </button>
+      </div>
 
       {workouts.map((workout) => (
         <section key={workout._id} className="flex flex-col justify-center m-4">
@@ -80,6 +137,12 @@ const Workouts: React.FC = () => {
               ))}
             </div>
           </Link>
+          <button
+            onClick={() => deleteWorkout(workout._id)}
+            className="text-red-500 font-bold"
+          >
+            Delete Workout
+          </button>
         </section>
       ))}
     </main>
